db6b2f667e155e2ec964fa3d0f85876a42843cc6
Nima Rafati <nimarafati@gmail.com>
2025-03-10 13:41:56 +0100

    Add mismatch introduction in find_probes.py script

--- codes/find_target.py
+++ codes/find_target.py
@@ -1,4 +1,3 @@
-
 from plp_directrna_design import probedesign as plp
 import pandas as pd
 import argparse
@@ -25,32 +24,34 @@ if __name__ == "__main__":
             formatter_class=argparse.RawTextHelpFormatter  # Ensures multiline formatting
     )
 
-def main(selected_features, fasta_file, output_file, plp_length, min_coverage, gc_min=50, gc_max=65, num_probes=10, iupac_mismatches=None):
+def main(selected_features, fasta_file, output_file, reference_fasta, min_coverage, gc_min=50, gc_max=65, num_probes=10, iupac_mismatches=None, max_errors = 1, check_specificity = False, plp_length=30):
     """
     Main function for probe extraction.
     """
     print(f"🔹 Loading selected features from {selected_features}...")
 
-    targets_df = plp.find_targets(selected_features = selected_features, fasta_file = fasta_file, 
+    print("DEBUG:", plp_length)
+    targets_df = plp.find_targets(selected_features = selected_features, fasta_file = fasta_file, reference_fasta = reference_fasta,
                                  plp_length = plp_length, min_coverage = min_coverage, output_file=output_file, 
-                                 gc_min=gc_min, gc_max=gc_max, num_probes=num_probes, iupac_mismatches=iupac_mismatches)
+                                 gc_min=gc_min, gc_max=gc_max, num_probes=num_probes, iupac_mismatches=iupac_mismatches,
+                                 max_errors=max_errors, check_specificity=check_specificity)
+    
     targets_df.to_csv(output_file, sep='\t', index=False)
-
-
-#parser.parse_args(["--help"])  # Simulate --help call for testing
 
 parser.add_argument("--selected_features", required=True, help="Path to the selected features file (TSV format)")
 parser.add_argument("--fasta_file", required=True, help="Path to the extracted sequences file (CDS/exons)")
 parser.add_argument("--output_file", required=True, help="Path to the output file")
-parser.add_argument("--plp_length", default=30, type=int, help="Minimum probe length")
+parser.add_argument("--reference_fasta", required=True, help="Path to the reference (genome/transcriptome) FASTA file; Please note that with genome reference, the probe design will be performed on the whole genome which may take a long time.")
 parser.add_argument("--min_coverage", default=1, type=int, help="Minimum coverage of the region")
 parser.add_argument("--gc_min", default=50, type=int, help="Minimum GC content")
 parser.add_argument("--gc_max", default=65, type=int, help="Maximum GC content")
 parser.add_argument("--num_probes", default=10, type=int, help="Number of probes to select")
 parser.add_argument("--iupac_mismatches", default=None, help="IUPAC mismatches to consider. Note that the number of mismatches should be less than or equal to 2. Example: 5:R,6:A")
+parser.add_argument("--max_errors", default=1, type=float, help="Maximum error rate (or number of errors if an integer, recommended range is 0-6)")
+parser.add_argument("--check_specificity", action="store_true", help="Check probe specificity")
+parser.add_argument("--plp_length", default=30, type=int, help="Minimum probe length")
 args = parser.parse_args()
-main(args.selected_features, args.fasta_file, args.output_file, args.plp_length, 
-     args.min_coverage, args.gc_min, args.gc_max, args.num_probes, args.iupac_mismatches)
 
-
-
+main(args.selected_features, args.fasta_file, args.output_file, args.reference_fasta, 
+     args.min_coverage, args.gc_min, args.gc_max, args.num_probes, 
+     args.iupac_mismatches, args.max_errors, args.check_specificity, args.plp_length)
